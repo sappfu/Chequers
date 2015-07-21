7d9a8f90d8a741b71f626571fec1db3c9000fdfa
jsapp1 <jsapp1@securustechnologies.com>
2015-07-21 07:41:25 -0500

    removed voice

--- Chequers/src/main/java/com/endovectors/uta/presentation/voice/VoiceTest.java
+++ Chequers/src/main/java/com/endovectors/uta/presentation/voice/VoiceTest.java
@@ -1,16 +1,12 @@
 package com.endovectors.uta.presentation.voice;
 
-import sun.audio.AudioPlayer;
-
-import javax.sound.sampled.AudioInputStream;
-import java.util.Set;
 
 /**
  * Created by asham_000 on 7/5/2015.
  */
-public class VoiceTest {
+public class VoiceTest{
 
     public void speak(String input){
 
-    }
+        }
 }
