78db808794bc113915234ed10d351b905bb64ce5
Patrick Break <patrick.break@mavs.uta.edu>
2015-07-23 08:22:05 -0500

    Initial code

---
+++ Chequers/src/main/java/com/endovectors/uta/presentation/voice/VoiceSelector.java
@@ -0,0 +1,18 @@
+package main.java.com.endovectors.uta.presentation.voice;
+
+public class VoiceSelector
+{
+	public VoiceSelector(int type)
+	{
+		// use interface for moves
+		// not sure of location
+		/*if (type == GOOD_MOVE)
+		{
+			// choose a good sound bite
+		}
+		else
+		{
+			// choose a bad sound bite
+		}*/
+	}
+}
