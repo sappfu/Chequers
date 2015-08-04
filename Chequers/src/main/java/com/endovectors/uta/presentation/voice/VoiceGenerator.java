ad6464b1585b5e20da7bfbf6eeb02f6df332d652
jsapp1 <jsapp1@securustechnologies.com>
2015-08-04 12:19:18 -0500

    voice gen

--- Chequers/src/main/java/com/endovectors/uta/presentation/voice/VoiceGenerator.java
+++ Chequers/src/main/java/com/endovectors/uta/presentation/voice/VoiceGenerator.java
@@ -4,16 +4,20 @@ package com.endovectors.uta.presentation.voice;
 import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 
 /**
  * Created by asham_000 on 7/5/2015.
  */
 public class VoiceGenerator {
 
+    VoiceManager vm = VoiceManager.getInstance();
+    Voice voice = vm.getVoice("kevin16");
+
     public void speak(String input){
         try {
-            VoiceManager vm = VoiceManager.getInstance();
-            Voice voice = vm.getVoice("kevin16");
             voice.allocate();
             voice.speak(input);
         }
