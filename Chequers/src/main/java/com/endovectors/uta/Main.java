1a1ef118ff158a009a7f7aa32283fec373d8fc71
John Sapp <john.sapp@mavs.uta.edu>
2015-08-12 14:21:33 -0500

    java library path

--- Chequers/src/main/java/com/endovectors/uta/Main.java
+++ Chequers/src/main/java/com/endovectors/uta/Main.java
@@ -14,6 +14,7 @@ public class Main {
     }
 
     private static void setup(){
+        System.out.println(System.getProperty("java.library.path"));
         System.setProperty("java.library.path", System.getProperty("user.dir"));
         masterController = new MasterController();
     }
