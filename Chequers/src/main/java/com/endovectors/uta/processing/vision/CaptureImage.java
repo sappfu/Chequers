97e3c589b1ba30f9fd90c32630b15bd454148a02
Patrick Break <patrick.break@mavs.uta.edu>
2015-08-12 16:45:15 -0500

    More changes to checkers squares

--- Chequers/src/main/java/com/endovectors/uta/processing/vision/CaptureImage.java
+++ Chequers/src/main/java/com/endovectors/uta/processing/vision/CaptureImage.java
@@ -152,7 +152,7 @@ public class CaptureImage {
 		int vOffset = vsegment + 40;
 		
 		// For angle of camera
-		int dx = 72;
+		int dx = 88;
 		//int ddx = 10;
 		hsegment -= 16;
 		
@@ -230,7 +230,7 @@ public class CaptureImage {
 				count = 0;
 				rowNum++;
 				hsegment += 2;
-				dx -= 9;
+				dx -= 11;
 				dy += 10;
 				vsegment += 3;
 			}
