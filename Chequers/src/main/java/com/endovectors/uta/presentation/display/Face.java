0756f0b36b4546fa28740dfd19795316a70c72b4
Patrick Break <patrick.break@mavs.uta.edu>
2015-08-13 16:05:21 -0500

    new face

--- Chequers/src/main/java/com/endovectors/uta/presentation/display/Face.java
+++ Chequers/src/main/java/com/endovectors/uta/presentation/display/Face.java
@@ -20,15 +20,17 @@ public class Face extends JPanel
 		g.setColor(Color.WHITE);
 		
 		//g.fillRect(width / 4, height / 10 + height / 4, width / 10, height / 60); // left eye
-		g.fillOval(width / 4 + width / 60, height / 10 + height / 4 + height / 20, width / 20, height / 20);
+		g.fillOval(width / 4 + width / 28, height / 10 + height / 4 + height / 28, width / 20, height / 20);
 		//g.fillRect(width - width / 4, height / 10 + height / 4, width / 10, height / 60); // right eye
-		g.fillOval(width - width / 4 - width / 60, height / 10 + height / 4 + height / 20, width / 20, height / 20);
+		g.fillOval(width - width / 4 + width / 28, height / 10 + height / 4 + height / 28, width / 20, height / 20);
 		
 		g.setColor(Color.BLACK);
 		
+		g.drawArc(width / 4, height / 10 + height / 4 - height / 20, width / 10, height / 40, 0, 145);
+		g.drawArc(width - width / 4 + width / 28, height / 10 + height / 4 - height / 20, width / 10, height / 40, 0, 145);
 		
-		g.fillOval(width / 4, height / 10 + height / 4, width / 40, height / 40); // left pupil
-		g.fillOval(width - width / 4, height / 10 + height / 4, width / 40, height / 40); // right pupil
+		g.fillOval(width / 4 + width / 20, height / 10 + height / 4 + height / 20, width / 40, height / 40); // left pupil
+		g.fillOval(width - width / 4 + width / 20, height / 10 + height / 4 + height / 20, width / 40, height / 40); // right pupil
 		
 		//g.fillRect(width / 4, height / 2 + height / 8, width / 2 + width / 10, height/ 75); // mouth
 		//g.setColor(Color.BLACK);
