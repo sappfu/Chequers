26f641b24007b95db85337b6e358ec8d1d875820
Patrick Break <patrick.break@mavs.uta.edu>
2015-08-12 22:20:25 -0500

    lower threshold

--- Chequers/src/main/java/com/endovectors/uta/processing/vision/CaptureImage.java
+++ Chequers/src/main/java/com/endovectors/uta/processing/vision/CaptureImage.java
@@ -512,7 +512,7 @@ public class CaptureImage {
 			Imgproc.cvtColor(roi, roi, Imgproc.COLOR_BGR2GRAY); // change to single color
 	        
 	        Mat canny = new Mat();
-	        Imgproc.Canny(roi, canny, 75, 150); // make image a canny image that is only edges; 2,4
+	        Imgproc.Canny(roi, canny, 50, 100); // make image a canny image that is only edges; 2,4
 	        // lower threshold values find more edges
 	        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 	        Mat hierarchy = new Mat(); // holds nested contour information
