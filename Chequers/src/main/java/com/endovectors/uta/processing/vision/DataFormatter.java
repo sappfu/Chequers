a6fd2b8b7106d6048f6cf577d4d3b00ec0892741
John Sapp <john.sapp@mavs.uta.edu>
2015-08-13 23:32:57 -0500

    crap

--- Chequers/src/main/java/com/endovectors/uta/processing/vision/DataFormatter.java
+++ Chequers/src/main/java/com/endovectors/uta/processing/vision/DataFormatter.java
@@ -1,8 +1,6 @@
 package com.endovectors.uta.processing.vision;
 
-import com.endovectors.uta.processing.BoardData;
 import com.endovectors.uta.processing.CheckersBoard;
-import com.endovectors.uta.processing.vision.DataConverter;
 
 /**
  * Created by asham_000 on 7/5/2015.
